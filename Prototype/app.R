2a375f690828a1b92a48def14d89d8ca119a287a
Desmond CHI A. <desmondchi100@gmail.com>
2025-02-11 14:43:53 +0100

    Accueil et couleurs

--- Prototype/app.R
+++ Prototype/app.R
@@ -12,9 +12,57 @@ error_message <- reactiveVal("")  # Message d'erreur pour le mot de passe
 
 # UI
 ui <- fluidPage(
+  style = "background-color: #ffe4b5; padding: 20px; border-radius: 10px;",
   titlePanel("Application Shiny avec Quiz et Buzzer"),
   
   tabsetPanel(
+    # Onglet 1 : Accueil
+    tabPanel("Accueil", 
+             style = "background-color: #fefee3; padding: 20px; border-radius: 10px;",
+             h2("Bienvenue dans l'application Quiz !"),
+             p("Notre application", strong(em("Buzzer")), "est une solution simple et interactive conçue pour faciliter et dynamiser vos jeux, quiz et compétitions. Elle permet aux utilisateurs d’appuyer sur un bouton virtuel pour déclencher un signal sonore, indiquant ainsi qui a répondu."),
+             p("Idéale pour des soirées entre amis, des événements ludiques ou des sessions de formation, cette application garantit une expérience fluide et équitable. Grâce à son interface intuitive et sa réactivité instantanée, elle transforme n'importe quel appareil en un véritable buzzer de jeu. 🚀"),
+             
+             # Mode d'emploi Administrateur
+             h3("Mode d'emploi Administrateur"),
+             p("En tant qu'administrateur, vous avez plusieurs fonctionnalités pour gérer le quiz et les joueurs :"),
+             tags$ul(
+               tags$li(strong("Ajouter des questions :"),
+                       "Vous pouvez ajouter de nouvelles questions à tout moment."),
+               tags$li(strong("Démarrer le jeu :"),
+                       "Cliquez sur le bouton 'Démarrer le jeu' pour commencer le quiz."),
+               tags$li(strong("Passer à la question suivante :"),
+                       "Vous pouvez faire défiler les questions en cliquant sur 'Question suivante'."),
+               tags$li(strong("Réinitialiser les buzzers :"),
+                       "Si nécessaire, vous pouvez réinitialiser l'ordre des buzzers."),
+               tags$li(strong("Voir l'ordre des buzzers :"),
+                       "L'ordre d'arrivée des joueurs au buzzer est affiché après chaque question."),
+               tags$li(strong("Bloquer ou exclure les buzzer :"),
+                       "Si nécessaire, vous pouvez bloquer ou exclure un buzzer."),
+               tags$li(strong("Télécharger votre questionnaire :"),
+                       "Vous pouvez obtenir un questionnaire vièrge à remplir en cliquant sur le button ci-dessous."),
+               # Bouton de téléchargement 
+               downloadButton("download_excel", "Télécharger un questionnaire vierge")
+             ),
+             
+             #Mode d'emploi Joueur 
+             h3("Mode d'emploi joueur"),
+             p("L'interface du mode joueur comprend une zone de texte pour entrer son nom, Une icône ", em("s'enregistrer")," & Un bouton", em(" buzzer.")),
+             h4("étapes"),
+             tags$ul(
+               tags$li(strong("Inscription :"),
+                       "Le joueur entre son nom dans la zone de texte et clique sur l’icône S’enregistrer pour valider sa participation. Sans cette étape, il ne pourra pas utiliser le buzzer."),
+               tags$li(strong("Utilisation du buzzer :"),
+                       "Une fois la question posée par le maître du jeu, le joueur peut appuyer sur le buzzer s’il connaît la réponse."),
+               tags$li(strong("Priorité au plus rapide :"),
+                       
+                       " Le premier joueur à appuyer est invité à répondre."),
+               tags$li(strong("Réinitialisation :"),
+                       "Seul le maître du jeu peut réinitialiser le buzzer.")
+             ),
+             h2("Bon jeux!!!", style = "text-align: center;")
+    ),
+    # Onglet 2 : Quiz et Buzzer
     tabPanel("Buzzer", 
              sidebarLayout(
                sidebarPanel(
@@ -177,6 +225,18 @@ server <- function(input, output, session) {
     # Suppression du message
     output$buzz_feedback <- renderText({"Vous pouvez buzzer !"})
   })
+  # Excel prérempli
+  output$download_excel <- downloadHandler(
+    filename = function() {
+      "questionnaires.xlsx"
+    },
+    content = function(file) {
+      # Création d'un dataframe d'exemple
+      df <- data.frame(Questions = "", stringsAsFactors = FALSE)
+      # Sauvegarde en fichier Excel
+      writexl::write_xlsx(df, file)
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
